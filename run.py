7ff1f5a00ae94854deddc50a053580816f0cb51f
ghyeon0 <xcm1321@gmail.com>
2021-11-13 13:13:29 +0900

    run.py: Code Cleanup

--- run.py
+++ run.py
@@ -3,9 +3,7 @@
 
 from simulator import Simulator
 
-
-sleep_ratios = [i for i in range(5, 105, 5)]
-update_periods = [1, 5, 10, 100, 1000] + [i for i in range(20, 100, 10)] + [i for i in range(200, 1000, 100)]
-sim = Simulator(log_folder="scenario3", sleep_ratios=sleep_ratios, update_periods=update_periods)
+# Setup Simulator
+sim = Simulator()
 
 sim.run()
